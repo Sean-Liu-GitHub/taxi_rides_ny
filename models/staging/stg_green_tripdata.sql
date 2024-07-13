4a3e3fecce0937b1a240dc8450f5c776d2a01dc2
Sean Liu <sean.liu.job@gmail.com>
2024-07-13 03:26:48 +0000

    update staging table

--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -11,7 +11,7 @@ select
 -- identifiers
     {{ dbt_utils.generate_surrogate_key(['vendorid','lpep_pickup_datetime']) }} as tripid,
     cast(vendorid as integer) as vendorid,
-    cast(ratecodeid as integer) as ratecodeid,
+    cast(PARSE_NUMERIC(ratecodeid) as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
     
@@ -23,7 +23,7 @@ select
     store_and_fwd_flag,
     cast(passenger_count as integer) as passenger_count,
     cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+    cast(PARSE_NUMERIC(trip_type) as integer) as trip_type,
     
     -- payment info
     cast(fare_amount as numeric) as fare_amount,
@@ -34,7 +34,7 @@ select
     cast(ehail_fee as numeric) as ehail_fee,
     cast(improvement_surcharge as numeric) as improvement_surcharge,
     cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
+    cast(PARSE_NUMERIC(payment_type) as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description
 
 from tripdata
